47cd6a9d96509a80dc631c4c6d2109d1589a8259
SlamZDank <83764499+SlamZDank@users.noreply.github.com>
2022-02-10 13:04:18 +0100

    Removed Being Sussy
    actually consider getting the version from the playstore as it will get more updates

--- app/src/main/java/com/draco/ladb/views/MainActivity.kt
+++ app/src/main/java/com/draco/ladb/views/MainActivity.kt
@@ -139,7 +139,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.abiUnsupportedDialog(badAbiDialog)
-        viewModel.piracyCheck(this)
+        //viewModel.piracyCheck(this)
     }
 
     /**
